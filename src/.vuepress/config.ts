e8a56b2541be1e818c638936f63623e5c5757419
yandixuan <451868992@qq.com>
2021-03-12 01:08:35 +0800

    first commit

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,55 @@
+import type { UserConfig, DefaultThemeOptions } from 'vuepress'
+
+const config: UserConfig<DefaultThemeOptions>={
+  bundlerConfig:{
+    chainWebpack:config=> {
+      config.resolve.alias.set('@image','/public/image/')
+    },
+  },
+  title: '阅读笔记',
+  description: '',
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    navbar: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          text: 'Guide',
+          isGroup: true,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
+export = config;
+
