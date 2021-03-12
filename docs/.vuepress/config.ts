5cf919686711c6d1f2e7780d5f486ace2a1fa848
yandixuan <451868992@qq.com>
2021-03-12 09:39:40 +0800

    chore: 调整目录

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,9 +1,9 @@
-import type { UserConfig, DefaultThemeOptions } from 'vuepress'
+import type { UserConfig, DefaultThemeOptions } from 'vuepress';
 
-const config: UserConfig<DefaultThemeOptions>={
-  bundlerConfig:{
-    chainWebpack:config=> {
-      config.resolve.alias.set('@image','/public/image/')
+const config: UserConfig<DefaultThemeOptions> = {
+  bundlerConfig: {
+    chainWebpack: (config) => {
+      config.resolve.alias.set('@image', '/public/image/');
     },
   },
   title: '阅读笔记',
@@ -11,7 +11,7 @@ const config: UserConfig<DefaultThemeOptions>={
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ],
   themeConfig: {
     repo: '',
@@ -26,30 +26,23 @@ const config: UserConfig<DefaultThemeOptions>={
       },
       {
         text: 'Config',
-        link: '/config/'
+        link: '/config/',
       },
       {
         text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
-      }
+        link: 'https://v1.vuepress.vuejs.org',
+      },
     ],
     sidebar: {
       '/guide/': [
         {
           text: 'Guide',
           isGroup: true,
-          children: [
-            '',
-            'using-vue',
-          ]
-        }
+          children: ['', 'using-vue'],
+        },
       ],
-    }
+    },
   },
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
-}
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
+};
 export = config;
-
